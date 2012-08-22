0a7b1bf6625cfd7eb8ffa96374f198064aa95e25
zykhan <zykhan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-22 21:03:23 +0000

    git-svn-id: https://plugins.svn.wordpress.org/yahoo-weather-plugin/trunk@589007 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,155 @@
+<?php
+/*
+Plugin Name: yahoo weather plugin
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Displying yahoo weather
+Version: 1.0
+Author: Muhammad Yasir khan
+Author URI: http://zykhan.wordpress.com
+License: Under GPL2
+*/
+/*
+	Copyright 2012 zykhan (email : sir_yasir@hotmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+error_reporting(E_ALL & ~E_NOTICE);
+ini_set('display_errors', 1);
+add_action("admin_menu","weather_code_add_page");
+
+function weather_code_add_page(){
+
+add_options_page('Weather Options','Weather Options','manage_options','displaying_weather_options','weather_dis_options_page');
+}
+           function weather_dis_options_page(){
+                        ?>
+         <div class="wrap">
+
+           <?php screen_icon(); ?>
+
+            <h1> Weather Plugin</h1>
+           <form action="" method="post">
+               <p>Enter Location code/ID:<input name="yw1" id="yw1" type="text" value="<?php echo $_POST['yw1'] ?>" /></p>
+               <p>Select tempreature unit:<select name="unit">
+               <option value="c" name="yw2">C</option>
+               <option value="f" name="yw2">F</option>
+               </select></p>
+             <input type="submit" name="submit" value="Save Changes" />
+            <input type="reset" name="reset" value="Reset" />
+            <p>You can search Location codes/IDs from <a href="http://www.edg3.co.uk/snippets/weather-location-codes/">here</a></p>
+               </form>
+
+            </div>
+            <?php
+            //global
+            //$yw1=$_POST['yw1'];
+            $options=array('city'=>$_POST['yw1'],'temp'=>$_POST['unit']);
+            //print_r($options);
+            //echo $yw1;
+
+
+            if(isset($_POST['submit'])){
+             if(empty($_POST['yw1'])){ ?>
+               <p><div id="message" class="error"> Please Enter City Code</div></p>
+             <?php }else{
+                $yw1=$_POST['yw1'];
+                add_option('city_unit', $options);
+           update_option('city_unit', $options);?>
+           <p><div id="message" class="updated">Settings saved successfully </div></p>
+            <?php  }
+            }
+            }
+
+
+
+
+            //add_action ("the_post", "yw");
+            add_action('wp_head', 'my_ss');
+            function my_ss(){
+             wp_register_style('my_css', plugins_url('style.css',__FILE__ ));
+             wp_enqueue_style('my_css');
+             }
+
+            add_shortcode('ywsc', 'yw');
+            function yw(){
+            $yw1 = get_option('city_unit');
+            $city=$yw1['city'];
+            $temp=$yw1['temp'];
+            if(empty($yw1)){
+            echo "Invalid or Empty weather location";
+            }
+   $doc = new DOMDocument();
+   $doc->load('http://weather.yahooapis.com/forecastrss?p='.$city.'&u='.$temp.'');
+    if(!$doc){
+    echo "Yahoo Weather not responding";
+    }
+    //now I get all elements inside this document with the following name "channel", this is the 'root'
+    $channel = $doc->getElementsByTagName("channel");
+    //now I go through each item withing $channel
+
+    foreach($channel as $chnl)
+    {
+             //I then find the 'item' element inside that loop
+    $item = $chnl->getElementsByTagName("item");
+    foreach($item as $itemgotten)
+    {
+    //now I search within '$item' for the element "description"
+    $describe = $itemgotten->getElementsByTagName("description");
+    //once I find it I create a variable named "$description" and assign the value of the Element to it
+   // foreach($describe as $des){
+  //    print_r($des->childNodes->item(2)->nodeValue);
+
+  //  }
+
+    $description = $describe->item(0)->nodeValue;
+    $icon=preg_match('/src="(.*?)"/i',$description,$im);
+    $loc['ico']=$im[0];
+
+
+    //and display it on-screen
+
+    //print_r($description);
+    $yw_arr=array('location','units','condition');
+               if($chnl->hasAttribute('city')){
+           echo "Empty element found";
+           }
+            $loc[] = $chnl->getElementsByTagNameNS("http://xml.weather.yahoo.com/ns/rss/1.0","$yw_arr[0]")->item(0)->getAttribute('city');
+            $loc[] = $chnl->getElementsByTagNameNS("http://xml.weather.yahoo.com/ns/rss/1.0","$yw_arr[0]")->item(0)->getAttribute('country');
+            $loc[] = $chnl->getElementsByTagNameNS("http://xml.weather.yahoo.com/ns/rss/1.0","$yw_arr[2]")->item(0)->getAttribute('temp');
+            $loc[] = $chnl->getElementsByTagNameNS("http://xml.weather.yahoo.com/ns/rss/1.0","$yw_arr[1]")->item(0)->getAttribute('temperature');
+            $loc[] = $chnl->getElementsByTagNameNS("http://xml.weather.yahoo.com/ns/rss/1.0","$yw_arr[2]")->item(0)->getAttribute('text');
+            //$loc[]=$tit->item(0)->getAttribute('city');
+           //$loc[]=$tit->item(0)->getAttribute('temp');
+            ?>
+
+            <?php
+
+            $final=$loc[0].",".$loc[1]." |";
+              //print_r($yw_arr);
+
+
+              $final2= "<span class=\"box\"><img width=\"33\" height=\"33\" title=\" $loc[4] \" $loc[ico] /></span> ";
+              $final1=$loc[2]."&#176"." ".$loc[3]."".$loc[5];
+             // return $final2;
+               ?>
+
+                <?php
+                return $final. $final2 . $final1;
+                    }
+    }
+
+
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,50 @@
+=== Yahoo! Weather Plugin ===
+Credit: Yahoo! Weather API
+Tags: weather, yahoo, feed, 
+Requires at least: 3.2.1
+Tested up to: 3.4.1
+Stable tag: 1.0
+
+
+== Description ==
+
+pUT your city weather in post/page or widget. Easy to customize and support multi-widget.
+
+Options
+
+Admin Panel
+Temperature Options
+Easly customizable Stylesheet
+
+
+Credits
+Yahoo Weather
+Weather Location Codes/IDs 
+
+
+== Installation ==
+
+You can use the built-in installer, or you can install the plugin manually.
+1. Go to the menu 'Plugins' -> 'Install' and search for 'Yahoo Weather plugin'
+2. Click 'install'.
+3. After installation check the setting menu dor option.
+
+==Usage==
+Use shorte code [ywsc] in your post/page.
+Use as widget write shortcode [ywsc] in text widget
+Note:if your theme does not support widget shortcode put  "add_filter('widget_text', 'do_shortcode')" in yout function.php file.
+
+== Frequently Asked Questions ==
+
+= A question that someone might have =
+
+== Screenshots ==
+
+1. Yahoo Weather in Widget
+2. Yahoo Weather in post
+3. Yahoo Weather Options
+
+== Changelog ==
+
+= 1.0 =
+* Released
