116cce5f7d019c3fec9e509c8673da1ae6ae91b3
zykhan <zykhan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-26 02:30:24 +0000

    git-svn-id: https://plugins.svn.wordpress.org/yahoo-weather-plugin/trunk@617608 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -100,7 +100,62 @@ add_options_page('Weather Options','Weather Options','manage_options','displayin
     //once I find it I create a variable named "$description" and assign the value of the Element to it
     $description = $describe->item(0)->nodeValue;
     $icon=preg_match('/src="(.*?)"/i',$description,$im);
-    $loc['ico']=$im[0];
+    preg_match('/[0-9]+\.[a-zA-Z]{2,5}/',$description,$new_ico);
+    //$loc['ico']=$im[0];
+
+    if($new_ico[0]==0){$loc[ico]=plugins_url('yahoo-weather-plugin/img/0.png');}
+    if($new_ico[0]==1){$loc[ico]=plugins_url('yahoo-weather-plugin/img/1.png');}
+    if($new_ico[0]==2){$loc[ico]=plugins_url('yahoo-weather-plugin/img/2.png');}
+    if($new_ico[0]==3){$loc[ico]=plugins_url('yahoo-weather-plugin/img/3.png');}
+    if($new_ico[0]==4){$loc[ico]=plugins_url('yahoo-weather-plugin/img/4.png');}
+    if($new_ico[0]==5){$loc[ico]=plugins_url('yahoo-weather-plugin/img/5.png');}
+    if($new_ico[0]==6){$loc[ico]=plugins_url('yahoo-weather-plugin/img/6.png');}
+    if($new_ico[0]==7){$loc[ico]=plugins_url('yahoo-weather-plugin/img/7.png');}
+    if($new_ico[0]==8){$loc[ico]=plugins_url('yahoo-weather-plugin/img/8.png');}
+    if($new_ico[0]==9){$loc[ico]=plugins_url('yahoo-weather-plugin/img/9.png');}
+
+    if($new_ico[0]==10){$loc[ico]=plugins_url('yahoo-weather-plugin/img/10.png');}
+    if($new_ico[0]==11){$loc[ico]=plugins_url('yahoo-weather-plugin/img/11.png');}
+    if($new_ico[0]==12){$loc[ico]=plugins_url('yahoo-weather-plugin/img/12.png');}
+    if($new_ico[0]==13){$loc[ico]=plugins_url('yahoo-weather-plugin/img/13.png');}
+    if($new_ico[0]==14){$loc[ico]=plugins_url('yahoo-weather-plugin/img/14.png');}
+    if($new_ico[0]==15){$loc[ico]=plugins_url('yahoo-weather-plugin/img/15.png');}
+    if($new_ico[0]==16){$loc[ico]=plugins_url('yahoo-weather-plugin/img/16.png');}
+    if($new_ico[0]==17){$loc[ico]=plugins_url('yahoo-weather-plugin/img/17.png');}
+    if($new_ico[0]==18){$loc[ico]=plugins_url('yahoo-weather-plugin/img/18.png');}
+    if($new_ico[0]==19){$loc[ico]=plugins_url('yahoo-weather-plugin/img/19.png');}
+    if($new_ico[0]==20){$loc[ico]=plugins_url('yahoo-weather-plugin/img/20.png');}
+
+    if($new_ico[0]==21){$loc[ico]=plugins_url('yahoo-weather-plugin/img/21.png');}
+    if($new_ico[0]==23){$loc[ico]=plugins_url('yahoo-weather-plugin/img/22.png');}
+    if($new_ico[0]==23){$loc[ico]=plugins_url('yahoo-weather-plugin/img/23.png');}
+    if($new_ico[0]==24){$loc[ico]=plugins_url('yahoo-weather-plugin/img/24.png');}
+    if($new_ico[0]==25){$loc[ico]=plugins_url('yahoo-weather-plugin/img/25.png');}
+    if($new_ico[0]==26){$loc[ico]=plugins_url('yahoo-weather-plugin/img/26.png');}
+    if($new_ico[0]==27){$loc[ico]=plugins_url('yahoo-weather-plugin/img/27.png');}
+    if($new_ico[0]==28){$loc[ico]=plugins_url('yahoo-weather-plugin/img/28.png');}
+    if($new_ico[0]==29){$loc[ico]=plugins_url('yahoo-weather-plugin/img/29.png');}
+    if($new_ico[0]==30){$loc[ico]=plugins_url('yahoo-weather-plugin/img/30.png');}
+
+    if($new_ico[0]==31){$loc[ico]=plugins_url('yahoo-weather-plugin/img/31.png');}
+    if($new_ico[0]==32){$loc[ico]=plugins_url('yahoo-weather-plugin/img/32.png');}
+    if($new_ico[0]==33){$loc[ico]=plugins_url('yahoo-weather-plugin/img/33.png');}
+    if($new_ico[0]==34){$loc[ico]=plugins_url('yahoo-weather-plugin/img/34.png');}
+    if($new_ico[0]==35){$loc[ico]=plugins_url('yahoo-weather-plugin/img/35.png');}
+    if($new_ico[0]==36){$loc[ico]=plugins_url('yahoo-weather-plugin/img/36.png');}
+    if($new_ico[0]==37){$loc[ico]=plugins_url('yahoo-weather-plugin/img/37.png');}
+    if($new_ico[0]==38){$loc[ico]=plugins_url('yahoo-weather-plugin/img/38.png');}
+    if($new_ico[0]==39){$loc[ico]=plugins_url('yahoo-weather-plugin/img/39.png');}
+    if($new_ico[0]==40){$loc[ico]=plugins_url('yahoo-weather-plugin/img/40.png');}
+
+    if($new_ico[0]==41){$loc[ico]=plugins_url('yahoo-weather-plugin/img/41.png');}
+    if($new_ico[0]==42){$loc[ico]=plugins_url('yahoo-weather-plugin/img/42.png');}
+    if($new_ico[0]==43){$loc[ico]=plugins_url('yahoo-weather-plugin/img/43.png');}
+    if($new_ico[0]==44){$loc[ico]=plugins_url('yahoo-weather-plugin/img/44.png');}
+    if($new_ico[0]==45){$loc[ico]=plugins_url('yahoo-weather-plugin/img/45.png');}
+    if($new_ico[0]==46){$loc[ico]=plugins_url('yahoo-weather-plugin/img/46.png');}
+    if($new_ico[0]==47){$loc[ico]=plugins_url('yahoo-weather-plugin/img/47.png');}
+
     //and display it on-screen
     //print_r($description);
     $yw_arr=array('location','units','condition');
@@ -113,10 +168,10 @@ add_options_page('Weather Options','Weather Options','manage_options','displayin
             $loc[] = $chnl->getElementsByTagNameNS("$yw_url","$yw_arr[2]")->item(0)->getAttribute('temp');
             $loc[] = $chnl->getElementsByTagNameNS("$yw_url","$yw_arr[1]")->item(0)->getAttribute('temperature');
             $loc[] = $chnl->getElementsByTagNameNS("$yw_url","$yw_arr[2]")->item(0)->getAttribute('text');
-            $final=$loc[0].",".$loc[1]." |";
-            $final2= "<span class=\"box\"><img width=\"33\" height=\"33\" title=\" $loc[4] \" $loc[ico] /></span> ";
-              $final1=$loc[2]."&#176"." ".$loc[3]."".$loc[5];
-                return $final. $final2 . $final1;
+            $final='<span style="color:#ffffff;">'.$loc[0].",".$loc[1].'</span>'  ." | ";
+            $final2= "<span class=\"box\"><img width=\"50\" height=\"45\" title=\" $loc[4] \" src=\"$loc[ico]\" /></span> ";
+              $final1='<span class="temp_text">'.$loc[2]."&#176"." ".$loc[3].'</span>';
+                return '<div class="w_box">'.$final. $final2 . $final1.'</div>';
             }
     }
 }
